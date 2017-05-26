70573fd1ca729caade4613d6312896fc82c67c54
Eric <Eric@192.168.1.41>
2017-05-26 12:13:36 +0200

    t

--- test-webapp/src/main/java/test/config/WebMvcConfig.java
+++ test-webapp/src/main/java/test/config/WebMvcConfig.java
@@ -1,11 +1,20 @@
 package test.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> httpMessageConverters) {
+		httpMessageConverters.add(new MappingJacksonHttpMessageConverter());
+	}
 
 }
--- test-webapp/src/main/java/test/controller/HelloController.java
+++ test-webapp/src/main/java/test/controller/HelloController.java
@@ -1,21 +1,35 @@
 package test.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import test.dto.ContentMovieDTO;
+import test.dto.MovieDTORequest;
 import test.model.Movie;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/test")
+	@RequestMapping("/test")
 	@ResponseBody
 	public Movie test() {
 		Movie movie = new Movie();
 		movie.setId(1L);
 		movie.setMovieName("Iron Man");
 		return movie;
+	}
+
+	@RequestMapping(value = "testPost", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<ContentMovieDTO> testPost(@RequestBody MovieDTORequest request) {
+		ContentMovieDTO dto = new ContentMovieDTO();
+		dto.setMovieName(request.getMovieName());
+		return new ResponseEntity<ContentMovieDTO>(dto, HttpStatus.CREATED);
 	}
 
 }
